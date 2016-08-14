bad653f4ade98b1062927762df2097a9e7a8e60f
Khalid Omar Ali <khalid.ali@consultant.com>
2016-08-14 03:33:28 +0100

    Add final solution

--- searching/sorted_search.py
+++ searching/sorted_search.py
@@ -1,17 +1,18 @@
-def binary_search(target,listy,size):
-    middle = size//2
-    upper = size
-    lower = 0
-    if target == listy[middle]:
-        return middle
-    while target != listy[middle]:
-        if target < listy[middle]:
-            upper = middle
-            middle = (upper + lower) // 2
-        if target >  listy[middle]:
-            lower = middle
-            middle = (upper + lower) // 2
-        if target == listy[middle]:
-             return middle
-        if middle > size:
-             return -1
+def find(target, listy):
+
+    lo = 0
+    hi = len(listy) - 1
+
+    while lo <= hi:
+        mid = (hi + lo) // 2
+
+        if target == listy[mid]:
+            return mid
+
+        elif target < listy[mid]:
+            hi = mid - 1
+
+        else:
+            lo = mid + 1
+
+    return -1
